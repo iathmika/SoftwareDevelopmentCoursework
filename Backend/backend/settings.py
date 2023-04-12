cdc445e712980692770815a921b8e1833f3020f1
Yt Li <liyt510@163.com>
2023-04-12 14:41:23 +0100

    setting add

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -136,3 +136,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+SESSION_COOKIE_SAMESITE = 'Lax'
+CSRF_COOKIE_SAMESITE='Lax'
+CORS_ORIGIN_ALLOW_ALL = True
